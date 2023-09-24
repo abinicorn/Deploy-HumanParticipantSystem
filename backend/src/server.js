443dd747c651f88e217d4ebfd1bc3fe69a0d46a7
Zijia Liu <80761505+abinicorn@users.noreply.github.com>
2023-09-24 22:15:16 +1300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,15 +13,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
-
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors({
+    origin: 'https://human-participant-system-frontend.vercel.app', // Allow requests from this origin
+    methods: 'GET,PUT,POST,DELETE', // Allow the specified methods
+    allowedHeaders: 'Content-Type,Authorization', // Allow the specified headers
+ }));
 
 
 app.use(express.json({ limit: '50mb' }));

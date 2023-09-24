7491547b2748e116fe27a999d97be03033653ef5
Zijia Liu <80761505+abinicorn@users.noreply.github.com>
2023-09-24 21:15:09 +1300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,32 +12,41 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use((req, res, next) => {
+
+// app.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   res.setHeader("Access-Control-Allow-Credentials", true);
   // res.header('Access-Control-Expose-Headers', 'set-cookie');
-  next();
-});
+//   next();
+// });
 // Setup CORS (place this before your routes)
+
+res.header('Access-Control-Allow-Origin', 'https://human-participant-system-frontend.vercel.app');
+res.header('Access-Control-Allow-Credentials', 'true');
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.send();
+});
 
 // Setup body-parser
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const corsOptions = {
-  origin: "https://human-participant-system-frontend.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   origin: "https://human-participant-system-frontend.vercel.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-// Setup log4js
+// // Setup log4js
 const log4js = require("./utils/log4js");
 
 app.use(cookieParser());
